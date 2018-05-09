8f6510853f308671147156a94ad4f2fe889c6cff
Forest Sharp <forestcollinssharp@gmail.com>
2018-05-09 14:44:38 -0400

    changing path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
-  pathPrefix: '/ForestCSharp',
+  pathPrefix: '/gatsby-starter-blog',
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
